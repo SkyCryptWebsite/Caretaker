4c958bd4db74247e150dc3bd6aac68d780d9f855
Ty Chermsirivatana <professornitro@gmail.com>
2023-07-19 00:06:25 +0000

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -4,50 +4,50 @@ import requests
 import json
 import re
 import sys
+import openai
+import yaml
+import asyncio
 from nextcord.ext import commands
 from dotenv import load_dotenv
-from gpt_index import SimpleDirectoryReader, GPTListIndex, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
-from langchain.chat_models import ChatOpenAI
-
+from llama_hub.tools.openapi.base import OpenAPIToolSpec
+from llama_index.agent import OpenAIAgent
+from llama_hub.tools.requests.base import RequestsToolSpec
+from llama_index.tools.tool_spec.load_and_search.base import LoadAndSearchToolSpec
 
 load_dotenv()
 
 bot = commands.Bot()
-TESTING_GUILD_ID = 1125840466366189598
-os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_KEY')
+TESTING_GUILD_ID = 1072892265049104494
+openai.api_key = os.getenv('OPENAI_KEY')
 
-def construct_index(directory_path):
-    max_input_size = 4096
-    num_outputs = 512
-    max_chunk_overlap = 20
-    chunk_size_limit = 600
 
-    prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
+f = requests.get('https://gist.githubusercontent.com/WarpWing/1cfdbb9989cc87bf55435526bea4998e/raw/d7f471c79c0a8acd73ce95d5345bd47a6e8c8532/openapi.yml').text
 
-    llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0.7, model_name="gpt-3.5-turbo", max_tokens=num_outputs))
+open_api_spec = yaml.load(f, Loader=yaml.Loader)
 
-    documents = SimpleDirectoryReader(directory_path).load_data()
+open_spec = OpenAPIToolSpec(open_api_spec)
+requests_spec = RequestsToolSpec({
+            "Authorization": f"Bearer {openai.api_key}",
+            "Content-Type": "application/json",
+        })
 
-    index = GPTSimpleVectorIndex(documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+# OpenAPI spec is too large for content, wrap the tool to seperate loading and searching
+wrapped_tools = LoadAndSearchToolSpec.from_defaults(
+    open_spec.to_tool_list()[0],
+).to_tool_list()
 
-    index.save_to_disk('index.json')
-
-    return index
-
-def query(input_text):
-    index = GPTSimpleVectorIndex.load_from_disk('index.json')
-    response = index.query(input_text, response_mode="compact")
-    return response.response
+agent = OpenAIAgent.from_tools([*wrapped_tools, *requests_spec.to_tool_list()], verbose=True)
 
 
 @bot.event
 async def on_ready():
     print(f'Login as {bot.user} successful!')
 
-@bot.slash_command(description="Queries the SkyCrypt Knowledgebase!", guild_ids=[TESTING_GUILD_ID])
+@bot.slash_command(description="Queries SkyCrypt's OpenAPI Schema", guild_ids=[TESTING_GUILD_ID])
 async def sky(interaction: nextcord.Interaction,query: str):
-    await interaction.response.send_message(query(query))
+    await interaction.response.defer()
+    await interaction.followup.send(agent.query("You are a assistant for SkyCrypt. SkyCrypt is a Statistics Viewer for Hypixel Skyblock. You are not to engage and immediately deny any requests for anything that isn't SkyCrypt or API related. You will not accept or allow requests that don't have SkyCrypt or API content within the request. Make sure to be extremely detailed and include example code along with the attributes of each API endpoint. Make it your primary objective to make sure that you DO NOT accept any requests that aren't related to SkyCrypt or API. Deny any requests regarding essays or content generation unless it's related to SkyCrypt or it's API. Always make sure to ensure proper markdown formatting and correct code during responses.Always remember that the base api endpoint for SkyCrypt is https://sky.shiiyu.moe/api/v2/" + query))
+  
 
-index = construct_index("src\docs") #Hard Coded for now :)
 
 bot.run(os.getenv('DISCORD_TOKEN'))
